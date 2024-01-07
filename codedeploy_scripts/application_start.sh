9f1cf597a13bea9786c45e69c080bae677d78ba0
Ryzeson <ryzeson@gmail.com>
2024-01-07 13:16:23 -0500

    add chmod to application_start script

--- codedeploy_scripts/application_start.sh
+++ codedeploy_scripts/application_start.sh
@@ -2,5 +2,8 @@
 
 echo "Beginning application_start script"
 
+echo "Change the permissions of server.js so that systemd can access it"
+chmod +x server.js 
+
 echo "Start the application again via systemd"
 systemctl start running-service
