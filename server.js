c819c5337d01244819d99f99f38a9be809ecff17
Ryzeson <ryzeson@gmail.com>
2024-01-15 20:42:38 -0500

    write signup route using promises and async/await

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post("/login", function (req, res) {
                     var db_verified = result.rows[0].verified;
                     var form_password = req.body.password;
 
-                    bcrypt.compare(form_password, hash, function(err, result) {
+                    bcrypt.compare(form_password, hash, function (err, result) {
                         if (result && db_verified == 'Y') {
                             res.render("index", { username: usernmae });
                         }
@@ -68,12 +68,12 @@ app.post("/login", function (req, res) {
                                 reasonMsg = 'Your email is not verified yet. Please check your inbox for an email containing your verification link';
                             else
                                 reasonMsg = 'Your password is incorrect. Please try again';
-    
+
                             res.render("login_failure", { reasonMsg: reasonMsg });
                         }
                     });
 
-                    
+
                 })
                 .catch(queryError => {
                     // It is possible that the client was already released in the previous then block, so try/catch this release()
@@ -101,55 +101,148 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.post('/signup', (req, res) => {
-    // First check if this username (primary key) exists in the database. If it does, ask the user to pick a different username
-    pool.connect().then(client => {
+// Using promises
+// app.post('/signup', (req, res) => {
+//     var db_client;
+//     var errorMsg = "";
+//     // Establish a connection to the database, and query based on username
+//     pool.connect().then(client => {
+//         db_client = client;
+//         var query = "SELECT * FROM users WHERE username = $1";
+//         var params = [req.body.username];
+//         return client.query(query, params)
+//     }).then(result => {
+//         // First check if this username (primary key) exists in the database. If it does, ask the user to pick a different username
+//         if (result.rows.length > 0) {
+//             errorMsg = "Username is already taken. Please use a different username.";
+//             return Promise.reject(errorMsg);
+//         }
+//         else {
+//             // No matches found, username is not taken
+//             // Table also requires email to be unique, so check that the email is not taken either
+//             var emailQuery = "SELECT * FROM users WHERE email = $1";
+//             var emailParam = [req.body.email];
+//             return db_client.query(emailQuery, emailParam);
+//         }
+//     }).then(emailResult => {
+//         // Email is already taken
+//         if (emailResult.rows.length > 0) {
+//             errorMsg = "Email is already taken. Please use a different email.";
+//             return Promise.reject(errorMsg);
+//         }
+//         else {
+//             // Email is not taken. Insert this record into the table
+//             // Hash password
+//             bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+//                 //Insert into db
+//                 var insertQuery = "INSERT INTO users VALUES($1, $2, $3)";
+//                 var insertParams = [req.body.username, hash, req.body.email];
+//                 db_client.query(insertQuery, insertParams).then(result => {
+//                     console.log("Successfully inserted the record");
+//                     return res.sendFile(__dirname + '/signup_success.html');
+//                 })
+//                     .catch(err => console.log(err))
+//             });
+
+
+//             // Cobnstruct and send verification email
+//             var href = 'http://' + process.env.IPV4 + ':3000/verify?username=' + req.body.username;
+
+//             var message = 'Verification link: <a href="' + href + '"><button>Click Here</button></a>';
+
+//             var mailOptions = {
+//                 from: process.env.EMAIL,
+//                 to: req.body.email,
+//                 subject: 'Running App Verification Email',
+//                 html: message
+//             };
+
+//             transporter.sendMail(mailOptions, function (error, info) {
+//                 if (error) {
+//                     console.log(error);
+//                 } else {
+//                     console.log('Email sent: ' + info.response);
+//                 }
+//             });
+//         }
+//     }).catch(err => {
+//         console.error(err);
+//         res.render('signup', { text: errorMsg });
+//     })
+// })
+
+// Using Async / Await
+app.post("/signup", async (req, res) => {
+    var errorMsg = "";
+    // Establish a connection to the database, and query based on username
+    try {
+        var client = await pool.connect();
         var query = "SELECT * FROM users WHERE username = $1";
         var params = [req.body.username];
-        client.query(query, params).then(result => {
-            if (result.rows.length == 0) {
-                //No matches found, username is not taken. Insert into db and verification email
+        var result = await client.query(query, params);
+        // First check if this username (primary key) exists in the database. If it does, ask the user to pick a different username
+        var emailResult;
+        if (result.rows.length > 0) {
+            errorMsg = "Username is already taken. Please use a different username.";
+            throw errorMsg;
+        }
+        else {
+            // No matches found, username is not taken
+            // Table also requires email to be unique, so check that the email is not taken either
+            var emailQuery = "SELECT * FROM users WHERE email = $1";
+            var emailParam = [req.body.email];
+            emailResult = await client.query(emailQuery, emailParam);
+        }
 
-                // Hash password
-                bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-                    //Insert into db
-                    var insertQuery = "INSERT INTO users VALUES($1, $2, $3)";
-                    var insertParams = [req.body.username, hash, req.body.email];
-                    client.query(insertQuery, insertParams).then(result => {
-                        console.log("Successfully inserted the record");
-                        return res.sendFile(__dirname + '/signup_success.html');
-                    })
-                        .catch(err => console.log(err))
-                });
+        if (emailResult.rows.length > 0) {
+            errorMsg = "Email is already taken. Please use a different email.";
+            throw errorMsg;
+        }
+        else {
+            // Email is not taken. Insert this record into the table
+            // Hash password
+            bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+                //Insert into db
+                var insertQuery = "INSERT INTO users VALUES($1, $2, $3)";
+                var insertParams = [req.body.username, hash, req.body.email];
+                try {
+                    var result = await client.query(insertQuery, insertParams);
+                    console.log("Successfully inserted the record");
+                    return res.sendFile(__dirname + '/signup_success.html');
+                }
+                catch(err) {
+                    console.log(err);
+                }
+            });
 
 
-                // Send verification email
-                var href = 'http://' + process.env.IPV4 + ':3000/verify?username=' + req.body.username;
+            // Cobnstruct and send verification email
+            var href = 'http://' + process.env.IPV4 + ':3000/verify?username=' + req.body.username;
 
-                var message = 'Verification link: <a href="' + href + '"><button>Click Here</button></a>';
+            var message = 'Verification link: <a href="' + href + '"><button>Click Here</button></a>';
 
-                var mailOptions = {
-                    from: process.env.EMAIL,
-                    to: req.body.email,
-                    subject: 'Running App Verification Email',
-                    html: message
-                };
+            var mailOptions = {
+                from: process.env.EMAIL,
+                to: req.body.email,
+                subject: 'Running App Verification Email',
+                html: message
+            };
 
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                });
-            }
-            else {
-                res.render('signup', {text: 'Username was already taken'});
-            }
-        })
-
-    })
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        }
+    }
+    catch(err) {
+        console.error(err);
+        res.render('signup', { text: errorMsg });
+    }
 })
+
 
 app.get('/verify', (req, res) => {
     console.log(req.query);
